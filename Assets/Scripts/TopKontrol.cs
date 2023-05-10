c810bef483c8bae9d91bf54b1651771d7a9accaf
Yusufhan ERBAŞ <63731504+Yusufhan-Erbas@users.noreply.github.com>
2023-05-10 11:34:48 +0300

    Halkanın temel kontrolleri yazıldı

--- Assets/Scripts/TopKontrol.cs
+++ Assets/Scripts/TopKontrol.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class TopKontrol : MonoBehaviour
 {
     Rigidbody2D rb;
     public float ziplamaKuvveti = 3f;
     bool basildiMi = false;
 
+    public string mevcutRenk;
+    public Color topunRengi;
+    public Color turkuaz, sari, mor, pembe;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-
+    private void Start()
+    {
+        RastgeleRenkBelirle();
+    }
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -33,4 +40,43 @@ public class TopKontrol : MonoBehaviour
         }
         
     }
-}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "RenkTekeri")
+        {
+            RastgeleRenkBelirle();
+            Destroy(collision.gameObject);
+            return;
+        }
+        if (collision.tag != mevcutRenk)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void RastgeleRenkBelirle()
+    {
+        int rastgeleSayi = Random.Range(0, 4);//0-1-2-3
+        switch (rastgeleSayi)
+        {
+            case 0:
+                mevcutRenk = "Turkuaz";
+                topunRengi = turkuaz;
+                break;
+            case 1:
+                mevcutRenk = "Sari";
+                topunRengi = sari;
+                break;
+            case 2:
+                mevcutRenk = "Pembe";
+                topunRengi = pembe;
+                break;
+            case 3:
+                mevcutRenk = "Mor";
+                topunRengi = mor;
+                break;
+        }
+        GetComponent<SpriteRenderer>().color = topunRengi;
+    }
+}//Class
